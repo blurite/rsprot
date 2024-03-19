3d6423610df22eaca835df76cc4c758db01cb8e0
Kris <kristo190@hotmail.com>
2024-03-19 23:26:18 +0200

    feat: add a channel attribute to denote game message's incoming queue
    All decoded incoming messages, unless dropped by the message filter, will be added to this queue to be polled by the game-thread later.

--- protocol/src/main/kotlin/net/rsprot/protocol/channel/ChannelAttributes.kt
+++ protocol/src/main/kotlin/net/rsprot/protocol/channel/ChannelAttributes.kt
@@ -3,6 +3,8 @@ package net.rsprot.protocol.channel
 import io.netty.util.AttributeKey
 import net.rsprot.protocol.cryptography.StreamCipherPair
 import net.rsprot.protocol.filter.MessageFilter
+import net.rsprot.protocol.message.IncomingMessage
+import net.rsprot.protocol.queue.MessageQueue
 
 public object ChannelAttributes {
     public val STREAM_CIPHER_PAIR: AttributeKey<StreamCipherPair> =
@@ -10,4 +12,7 @@ public object ChannelAttributes {
 
     public val GAME_MESSAGE_FILTER: AttributeKey<MessageFilter> =
         AttributeKey.newInstance("prot_game_message_filter")
+
+    public val GAME_MESSAGE_INCOMING_QUEUE: AttributeKey<MessageQueue<IncomingMessage>> =
+        AttributeKey.newInstance("prot_game_message_incoming_queue")
 }
