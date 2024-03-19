95b6e36428810e180377bf339e2f13bd673eb12d
Kris <kristo190@hotmail.com>
2024-03-19 22:45:31 +0200

    feat: add support for MessageFilters, with a supplied implementation of DroppingMessageFilter
    These filters are responsible for determining whether an incoming packet (client -> server) will be accepted or not. If the filter returns MessageFilter#DROP_MESSAGE, the bytes are dropped altogether and no slicing + decoding will take place. The MessageFilter#accept function returns an integer which will be passed onto message queues (to be implemented later), allowing users to define custom behaviors at specific thresholds, should they so desire.

--- protocol/src/main/kotlin/net/rsprot/protocol/channel/ChannelAttributes.kt
+++ protocol/src/main/kotlin/net/rsprot/protocol/channel/ChannelAttributes.kt
@@ -2,8 +2,12 @@ package net.rsprot.protocol.channel
 
 import io.netty.util.AttributeKey
 import net.rsprot.protocol.cryptography.StreamCipherPair
+import net.rsprot.protocol.filter.MessageFilter
 
 public object ChannelAttributes {
     public val STREAM_CIPHER_PAIR: AttributeKey<StreamCipherPair> =
         AttributeKey.newInstance("prot_stream_cipher_pair")
+
+    public val GAME_MESSAGE_FILTER: AttributeKey<MessageFilter> =
+        AttributeKey.newInstance("prot_game_message_filter")
 }
