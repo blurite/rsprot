b67d9a2940e11a83ac70b11c3e210140fa7fbac8
Kris <kristo190@hotmail.com>
2024-03-30 20:05:34 +0200

    feat: add documentation for ObserverExtendedInfoFlags, additionally change it up to not utilize a BitSet as that turns out to be much slower

--- protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/playerinfo/util/ObserverExtendedInfoFlags.kt
+++ protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/playerinfo/util/ObserverExtendedInfoFlags.kt
@@ -1,31 +1,47 @@
 package net.rsprot.protocol.game.outgoing.info.playerinfo.util
 
-import java.util.BitSet
-
+/**
+ * A data structure holding information about observer-dependent extended info flags.
+ * An example of this would be any extended info blocks that get written when an avatar is
+ * moved from low resolution to high resolution, in which case we need to synchronize any
+ * data that was set in the past, such as their appearance, the move speed and
+ * the face pathingentity status. This additionally includes any extended info blocks which
+ * were flagged for a specific observer alone, such as tinting utilized in Tombs of Amascut,
+ * where a single user will see tinting applied to all the other members of the party.
+ * When setting up the tinting, rather than flagging tinting on the recipient,
+ * we flag the observer-dependent flag on the receiver of the given extended info block.
+ */
 internal class ObserverExtendedInfoFlags(capacity: Int) {
-    private val observerExtendedInfoFlags: IntArray = IntArray(capacity)
-    private val flaggedAvatars: BitSet = BitSet(capacity)
+    /**
+     * The observer-dependent flags. This array will not include "static" flags.
+     */
+    private val flags: IntArray = IntArray(capacity)
 
+    /**
+     * Resets the observer-dependent flags by filling the array with zeros.
+     */
     fun reset() {
-        var nextSetBit = flaggedAvatars.nextSetBit(0)
-        if (nextSetBit == -1) {
-            return
-        }
-        do {
-            observerExtendedInfoFlags[nextSetBit] = 0
-            nextSetBit = flaggedAvatars.nextSetBit(nextSetBit + 1)
-        } while (nextSetBit != -1)
-        flaggedAvatars.clear()
+        flags.fill(0)
     }
 
+    /**
+     * Appends the given [flag] for avatar at index [index].
+     * @param index the index of the recipient player
+     * @param flag the bit flag to enable
+     */
     fun addFlag(
         index: Int,
         flag: Int,
     ) {
-        observerExtendedInfoFlags[index] = observerExtendedInfoFlags[index] or flag
+        flags[index] = flags[index] or flag
     }
 
+    /**
+     * Gets the observer-dependent flag of the avatar at index [index]
+     * @param index the index of the recipient player
+     * @return the observer-dependent flag value
+     */
     fun getFlag(index: Int): Int {
-        return observerExtendedInfoFlags[index]
+        return flags[index]
     }
 }
