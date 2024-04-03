0e5d3516c20e16c0548322974c6001eac72aad31
Kris <kristo190@hotmail.com>
2024-04-03 09:41:43 +0300

    fix: shorten chat length in player info benchmarks, as it exceeded the realistic capacity

--- protocol/osrs-221-desktop/src/benchmarks/kotlin/net/rsprot/protocol/game/outgoing/info/PlayerInfoBenchmark.kt
+++ protocol/osrs-221-desktop/src/benchmarks/kotlin/net/rsprot/protocol/game/outgoing/info/PlayerInfoBenchmark.kt
@@ -104,8 +104,7 @@ class PlayerInfoBenchmark {
                 0,
                 0,
                 false,
-                "Neque porro quisquam est qui dolorem " +
-                    "ipsum quia dolor sit amet, consectetur, adipisci velit",
+                "Neque porro quisquam est qui dolorem ipsum quia do",
                 null,
             )
         }
