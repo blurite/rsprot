f5ea8792b27ec355898a729df69b4341c42c927c
Kris <kristo190@hotmail.com>
2024-04-11 23:57:37 +0300

    feat: logout packets

---
+++ protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/logout/LogoutWithReason.kt
@@ -0,0 +1,40 @@
+package net.rsprot.protocol.game.outgoing.logout
+
+import net.rsprot.protocol.message.OutgoingMessage
+
+/**
+ * Logout with reason, much like [Logout], is used to
+ * log the player out of the game. The only difference here
+ * is that the user will be given a reason for why they were
+ * logged out of the game, e.g. inactive for too long.
+ *
+ * Logout reasons table:
+ * ```
+ * | Id |   Type   |
+ * |----|:--------:|
+ * | 1  |  Kicked  |
+ * | 2  | Updating |
+ * ```
+ *
+ * @property reason the id of the reason to display (see table above)
+ */
+public class LogoutWithReason(
+    public val reason: Int,
+) : OutgoingMessage {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LogoutWithReason
+
+        return reason == other.reason
+    }
+
+    override fun hashCode(): Int {
+        return reason
+    }
+
+    override fun toString(): String {
+        return "LogoutWithReason(reason=$reason)"
+    }
+}
