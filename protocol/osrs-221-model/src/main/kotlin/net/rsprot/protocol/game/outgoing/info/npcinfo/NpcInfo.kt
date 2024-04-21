4a9ed1bb64ce24e1c7efa98e91f833f43502f75c
Kris <kristo190@hotmail.com>
2024-04-21 05:58:40 +0300

    docs: add documentation for npc info class

--- protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/npcinfo/NpcInfo.kt
+++ protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/npcinfo/NpcInfo.kt
@@ -15,6 +15,24 @@ import net.rsprot.protocol.shared.platform.PlatformType
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
+/**
+ * An implementation of the npc info packet.
+ * This class is responsible for bringing together all the bits of the npc info protocol,
+ * including copying all the pre-built buffers that were made beforehand.
+ * @property allocator the byte buffer allocator used to allocate new buffers to be used
+ * for the npc info packet, as well as the pre-built extended info buffers.
+ * @property repository the npc avatar repository, keeping track of every npc avatar that exists
+ * in the game.
+ * @property platformType the platform the player owning this npc info packet is on
+ * @property localPlayerIndex the index of the local player that owns this npc info packet.
+ * @property indexSupplier a supplier-style interface responsible for yielding npc indices
+ * which are within vicinity of the player. This is the primary way the server will be providing
+ * information about nearby NPCs to a player, as well as whether to render the NPC in the first place,
+ * as some NPCs are meant to only render to a given player if certain conditions are met.
+ * @property lowResolutionToHighResolutionEncoders a platform map of low resolution to high resolution
+ * change encoders, used to move a npc into high resolution for the given player.
+ * As this is scrambled, a separate platform-specific implementation is required.
+ */
 @Suppress("ReplaceUntilWithRangeUntil")
 @ExperimentalUnsignedTypes
 public class NpcInfo internal constructor(
@@ -25,17 +43,55 @@ public class NpcInfo internal constructor(
     private val indexSupplier: NpcIndexSupplier,
     private val lowResolutionToHighResolutionEncoders: PlatformMap<NpcResolutionChangeEncoder>,
 ) : ReferencePooledObject, OutgoingMessage {
+    /**
+     * The last cycle's coordinate of the local player, used to perform faster npc removal.
+     * If the player moves a greater distance than the [viewDistance], we can make the assumption
+     * that all the existing high-resolution NPCs need to be removed, and thus remove them
+     * in a simplified manner, rather than applying a coordinate check on each one. This commonly
+     * occurs whenever a player teleports far away.
+     */
     private var localPlayerLastCoord: CoordGrid = CoordGrid.INVALID
+
+    /**
+     * The current coordinate of the local player used for the calculations of this npc info
+     * packet. This will be cross-referenced against NPCs to ensure they are within distance.
+     */
     private var localPlayerCurrentCoord: CoordGrid = CoordGrid.INVALID
+
+    /**
+     * The maximum view distance how far a player will see other NPCs.
+     * Unlike with player info, this does not automatically resize to accommodate for nearby NPCs,
+     * as it is almost impossible for such a scenario to happen in the first place.
+     * It is confirmed that OldSchool RuneScape does not do it either.
+     */
     private var viewDistance: Int = MAX_SMALL_PACKET_DISTANCE
+
+    /**
+     * The indices of the high resolution NPCs, in the order as they came in.
+     * This is a replica of how the client keeps track of NPCs.
+     */
     private var highResolutionNpcIndices: UShortArray =
         UShortArray(MAX_HIGH_RESOLUTION_NPCS) {
-            UNSIGNED_MAX_SHORT
+            NPC_INDEX_TERMINATOR
         }
+
+    /**
+     * A secondary array for high resolution NPCs.
+     * After each cycle, the [highResolutionNpcIndices] gets swapped with this property,
+     * and the indices will be appended one by one. As a result of it, we can get away
+     * with significantly fewer operations to defragment the array, as we don't have to
+     * shift every entry over, we only need to fill in the ones that still exist.
+     */
     private var temporaryHighResolutionNpcIndices: UShortArray =
         UShortArray(MAX_HIGH_RESOLUTION_NPCS) {
-            UNSIGNED_MAX_SHORT
+            NPC_INDEX_TERMINATOR
         }
+
+    /**
+     * A counter for how many high resolution NPCs are currently being tracked.
+     * This count cannot exceed [MAX_HIGH_RESOLUTION_NPCS], as the client
+     * only supports that many extended info updates.
+     */
     private var highResolutionNpcIndexCount: Int = 0
 
     /**
@@ -51,20 +107,26 @@ public class NpcInfo internal constructor(
     private var extendedInfoCount: Int = 0
 
     /**
-     * The observer info flags are used for us to track extended info blocks which weren't necessarily
-     * flagged on the target player. This can happen during the transitioning from low resolution
-     * to high resolution, in which case appearance, move speed and face pathingentity may be transmitted,
-     * despite not having been flagged. Additionally, some extended info blocks, such as hits and tinting,
-     * will sometimes be observer-dependent. This means each observer will receive a different variant
-     * of the extended info buffer. A simple example of this is the red circle hitmark ironmen will
-     * see on NPCs whenever they attack a NPC that has already received damage from another player.
-     * Only the ironman will receive information about that hitmark in this case, and no one else.
+     * The observer extended info flags are a means to track which extended info blocks
+     * we need to transmit when moving a NPC from low resolution to high resolution,
+     * as there are numerous extended info blocks which hold state over a long period
+     * of time, such as head icon changes - if we didn't do this, anyone that observes
+     * a NPC after the cycle during which the head icons were set, would not see these
+     * head icons.
      */
     private val observerExtendedInfoFlags: ObserverExtendedInfoFlags =
         ObserverExtendedInfoFlags(MAX_HIGH_RESOLUTION_NPCS)
 
+    /**
+     * The primary npc info buffer, holding all the bitcodes and extended info blocks.
+     */
     private var buffer: ByteBuf? = null
 
+    /**
+     * Returns the backing byte buffer holding all the computed information.
+     * @throws IllegalStateException if the buffer is null, meaning it has no yet been
+     * initialized for this cycle.
+     */
     @Throws(IllegalStateException::class)
     public fun backingBuffer(): ByteBuf {
         return checkNotNull(buffer)
@@ -81,6 +143,14 @@ public class NpcInfo internal constructor(
         return buffer
     }
 
+    /**
+     * Updates the coordinate of the local player, as this is necessary to know
+     * how far NPCs nearby are to the player, which allows us to remove NPCs that
+     * have gone too far out, and add NPCs that are within certain distance.
+     * @param level the height level of the local player
+     * @param x the x coordinate of the local player
+     * @param z the z coordinate of the local player
+     */
     public fun updateCoord(
         level: Int,
         x: Int,
@@ -94,6 +164,11 @@ public class NpcInfo internal constructor(
             )
     }
 
+    /**
+     * Computes the high resolution and low resolution bitcodes for this given player,
+     * additionally marks down which NPCs need to furthermore send their extended info
+     * updates.
+     */
     public fun compute() {
         val viewDistance = this.viewDistance
         val buffer = allocBuffer()
@@ -113,16 +188,26 @@ public class NpcInfo internal constructor(
         }
     }
 
+    /**
+     * Synchronizes the last coordinate of the local player with the current coordinate
+     * set previously in this cycle. This is simply to help make removal of all NPCs
+     * in high resolution more efficient, as we can avoid distance checks against every
+     * NPC, and only do so against the player's last coordinate.
+     */
     public fun afterUpdate() {
         this.localPlayerLastCoord = localPlayerCurrentCoord
     }
 
+    /**
+     * Writes the extended info blocks over to the backing buffer, based on the indices
+     * of the NPCs from whom we requested extended info updates prior in this cycle.
+     */
     internal fun putExtendedInfo() {
         val jagBuffer = backingBuffer().toJagByteBuf()
         for (i in 0 until extendedInfoCount) {
             val index = extendedInfoIndices[i].toInt()
             val other = checkNotNull(repository.getOrNull(index))
-            val observerFlag = observerExtendedInfoFlags.getFlag(i)
+            val observerFlag = other.extendedInfo.flags or observerExtendedInfoFlags.getFlag(i)
             other.extendedInfo.pExtendedInfo(
                 platformType,
                 jagBuffer,
@@ -134,7 +219,7 @@ public class NpcInfo internal constructor(
 
     /**
      * Performs an index defragmentation on the [highResolutionNpcIndices] array.
-     * This function will effectively take all indices that are NOT [UNSIGNED_MAX_SHORT]
+     * This function will effectively take all indices that are NOT [NPC_INDEX_TERMINATOR]
      * and put them into the [temporaryHighResolutionNpcIndices] in a consecutive order,
      * without gaps. Afterwards, the [temporaryHighResolutionNpcIndices] and [highResolutionNpcIndices]
      * arrays get swapped out, so our [highResolutionNpcIndices] becomes a defragmented array.
@@ -165,7 +250,7 @@ public class NpcInfo internal constructor(
                 break
             }
             val index = highResolutionNpcIndices[i]
-            if (index != UNSIGNED_MAX_SHORT) {
+            if (index != NPC_INDEX_TERMINATOR) {
                 temporaryHighResolutionNpcIndices[count++] = index
             }
         }
@@ -174,6 +259,18 @@ public class NpcInfo internal constructor(
         this.temporaryHighResolutionNpcIndices = uncompressed
     }
 
+    /**
+     * Processes high resolution, existing, NPCs by writing their movements/extended info updates,
+     * or removes them altogether if need be.
+     * @param buffer the buffer into which to write the bitcode information.
+     * @param viewDistance the maximum view distance how far a NPC can be seen.
+     * If the npc is farther away from the local player than the provided view distance,
+     * they will be removed from high resolution view.
+     * @return whether any high resolution npcs were removed in the middle of the
+     * array. This does not include the npcs dropped off at the end.
+     * This is necessary to determine whether we need to defragment the array (ie remove any
+     * gaps that were produced by removing npcs in the middle of the array).
+     */
     private fun processHighResolution(
         buffer: BitBuf,
         viewDistance: Int,
@@ -235,6 +332,13 @@ public class NpcInfo internal constructor(
         return processedCount != highResolutionNpcIndexCount
     }
 
+    /**
+     * Checks whether to remove a high resolution npc from the high resolution view.
+     * @param avatar the avatar of the high resolution npc, or null.
+     * @param viewDistance the maximum view distance how far the npc can be without
+     * being removed.
+     * @return whether to remove the npc from the high resolution view.
+     */
     @OptIn(ExperimentalContracts::class)
     private fun removeHighResolutionNpc(
         avatar: NpcAvatar?,
@@ -249,6 +353,14 @@ public class NpcInfo internal constructor(
             !withinDistance(localPlayerCurrentCoord, avatar.details.currentCoord, viewDistance)
     }
 
+    /**
+     * Checks if the player has moved a greater distance from their previous coordinate
+     * than the maximum [viewDistance], in which case all existing high resolution NPCs
+     * can be removed in one go in a more efficient manner.
+     * @param viewDistance the maximum view distance how far a player can see other npcs
+     * @return whether the player has moved a greater distance than [viewDistance] since
+     * the last cycle.
+     */
     private fun isTooFar(viewDistance: Int): Boolean {
         return !withinDistance(
             this.localPlayerLastCoord,
@@ -257,6 +369,17 @@ public class NpcInfo internal constructor(
         )
     }
 
+    /**
+     * Processes the NPCs that are in low resolution by requesting an iterator of NPC indices
+     * within [viewDistance] of the local player's current coordinate.
+     * This function is responsible for deciding which NPCs to move to high resolution,
+     * and ignore which ones are already in high resolution. It is the server's duty to always
+     * return all the NPCs that should be added, regardless of if they were previously already
+     * added.
+     * @param buffer the primary buffer into which to write the bitcode information
+     * @param viewDistance the maximum view distance how far a npc can be from the local
+     * player to still be considered in high resolution.
+     */
     private fun processLowResolution(
         buffer: BitBuf,
         viewDistance: Int,
@@ -276,8 +399,8 @@ public class NpcInfo internal constructor(
                 viewDistance,
             )
         while (npcs.hasNext()) {
-            val index = npcs.next() and MAX_USHORT
-            if (index == MAX_USHORT || isHighResolution(index)) {
+            val index = npcs.next() and NPC_INFO_CAPACITY
+            if (index == NPC_INFO_CAPACITY || isHighResolution(index)) {
                 continue
             }
             if (this.highResolutionNpcIndexCount >= MAX_HIGH_RESOLUTION_NPCS) {
@@ -300,6 +423,12 @@ public class NpcInfo internal constructor(
         }
     }
 
+    /**
+     * Checks whether a npc by the index of [index] is already within our high resolution
+     * view.
+     * @param index the index of the npc to check
+     * @return whether the npc at the given index is already in high resolution.
+     */
     private fun isHighResolution(index: Int): Boolean {
         // NOTE: Perhaps it's more efficient to just allocate 65535 bits and do a bit check?
         // Would cost ~16.76mb at max world capacity
@@ -311,6 +440,10 @@ public class NpcInfo internal constructor(
         return false
     }
 
+    /**
+     * Checks whether the [coord] is within [distance] of the [localPlayerCoordGrid].
+     * @return whether the coord is within distance of the local player's current coordinate.
+     */
     private fun withinDistance(
         localPlayerCoordGrid: CoordGrid,
         coord: CoordGrid,
@@ -344,9 +477,26 @@ public class NpcInfo internal constructor(
          * The default capacity of the backing byte buffer into which all player info is written.
          */
         private const val BUF_CAPACITY: Int = 40_000
+
+        /**
+         * The maximum view distance that can be transmitted using the smaller npc info packet.
+         */
         private const val MAX_SMALL_PACKET_DISTANCE: Int = 15
+
+        /**
+         * The maximum number of high resolution NPCs that the client supports, limited by the
+         * client's array of extended info updates being a size-250 int array.
+         */
         private const val MAX_HIGH_RESOLUTION_NPCS: Int = 250
-        private const val UNSIGNED_MAX_SHORT: UShort = 0xFFFFu
-        private const val MAX_USHORT = 0xFFFF
+
+        /**
+         * The terminator value used to indicate that no NPC is here.
+         */
+        private const val NPC_INDEX_TERMINATOR: UShort = 0xFFFFu
+
+        /**
+         * Maximum unsigned short constant, the capacity of the npc info protocol.
+         */
+        private const val NPC_INFO_CAPACITY = 0xFFFF
     }
 }
