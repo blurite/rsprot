f8a36a5e890cd9c2580ea95682adc67c2fddf37e
Kris <kristo190@hotmail.com>
2024-04-23 09:43:31 +0300

    feat: bind js5 decoders

--- protocol/osrs-221-shared/src/main/kotlin/net/rsprot/protocol/common/js5/incoming/prot/Js5MessageDecoderRepository.kt
+++ protocol/osrs-221-shared/src/main/kotlin/net/rsprot/protocol/common/js5/incoming/prot/Js5MessageDecoderRepository.kt
@@ -1,6 +1,11 @@
 package net.rsprot.protocol.common.js5.incoming.prot
 
 import net.rsprot.protocol.ProtRepository
+import net.rsprot.protocol.common.js5.incoming.codec.PrefetchRequestDecoder
+import net.rsprot.protocol.common.js5.incoming.codec.PriorityChangeHighDecoder
+import net.rsprot.protocol.common.js5.incoming.codec.PriorityChangeLowDecoder
+import net.rsprot.protocol.common.js5.incoming.codec.UrgentRequestDecoder
+import net.rsprot.protocol.common.js5.incoming.codec.XorChangeDecoder
 import net.rsprot.protocol.message.codec.incoming.MessageDecoderRepository
 import net.rsprot.protocol.message.codec.incoming.MessageDecoderRepositoryBuilder
 
@@ -10,6 +15,11 @@ public object Js5MessageDecoderRepository {
         val protRepository = ProtRepository.of<Js5ClientProt>()
         val builder =
             MessageDecoderRepositoryBuilder(protRepository).apply {
+                bind(PrefetchRequestDecoder())
+                bind(PriorityChangeHighDecoder())
+                bind(PriorityChangeLowDecoder())
+                bind(UrgentRequestDecoder())
+                bind(XorChangeDecoder())
             }
         return builder.build()
     }
