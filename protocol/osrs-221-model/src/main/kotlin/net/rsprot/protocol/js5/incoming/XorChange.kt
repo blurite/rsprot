e87357b2b9d99fc9f4ec27fbb5cb87573f625bd0
Kris <kristo190@hotmail.com>
2024-04-24 11:11:00 +0300

    fix: make key public in xor change

--- protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/js5/incoming/XorChange.kt
+++ protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/js5/incoming/XorChange.kt
@@ -3,5 +3,5 @@ package net.rsprot.protocol.js5.incoming
 import net.rsprot.protocol.message.IncomingMessage
 
 public data class XorChange(
-    private val key: Int,
+    public val key: Int,
 ) : IncomingMessage
