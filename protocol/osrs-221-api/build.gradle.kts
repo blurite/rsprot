5a26d433ea0bd8b1f65267cfd04b26dbec0636b2
Kris <kristo190@hotmail.com>
2024-04-26 16:52:39 +0300

    chore: make api module depend on various netty modules + buffer & shared modules

--- protocol/osrs-221-api/build.gradle.kts
+++ protocol/osrs-221-api/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 dependencies {
     implementation(platform(libs.netty.bom))
     api(libs.netty.buffer)
+    implementation(libs.netty.transport)
+    implementation(libs.netty.handler)
+    implementation(libs.netty.native.epoll)
+    implementation(libs.netty.native.kqueue)
+    implementation(libs.netty.incubator.iouring)
     api(projects.protocol)
     api(projects.compression)
     api(projects.crypto)
@@ -12,4 +17,6 @@ dependencies {
     api(projects.protocol.osrs221Model)
     implementation(projects.protocol.osrs221Internal)
     implementation(projects.protocol.osrs221Desktop)
+    implementation(projects.protocol.osrs221Shared)
+    implementation(projects.buffer)
 }
