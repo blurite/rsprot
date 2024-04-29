08024d0b0eb2d16ed79a383558d57e1344155735
Kris <kristo190@hotmail.com>
2024-04-29 14:38:12 +0300

    feat: introduce Huffman codec provider

---
+++ compression/src/main/kotlin/net/rsprot/compression/provider/DefaultHuffmanCodecProvider.kt
@@ -0,0 +1,14 @@
+package net.rsprot.compression.provider
+
+import net.rsprot.compression.HuffmanCodec
+
+/**
+ * A default Huffman codec provider that takes a preloaded codec in and returns it
+ * without any sort of extra steps.
+ * @property huffmanCodec the huffman codec to supply.
+ */
+public class DefaultHuffmanCodecProvider(private val huffmanCodec: HuffmanCodec) : HuffmanCodecProvider {
+    override fun provide(): HuffmanCodec {
+        return huffmanCodec
+    }
+}
