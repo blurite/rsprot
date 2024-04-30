41a4609a5892c8cb6e6e48608369f9c6e5019d31
Kris <kristo190@hotmail.com>
2024-04-30 20:22:42 +0300

    feat: add a zero-key xtea provider instance, for servers which zero-out any xtea keys

--- protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/map/util/XteaProvider.kt
+++ protocol/osrs-221-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/map/util/XteaProvider.kt
@@ -4,4 +4,9 @@ import net.rsprot.crypto.util.XteaKey
 
 public fun interface XteaProvider {
     public fun provide(mapsquareId: Int): XteaKey
+
+    public companion object {
+        @JvmStatic
+        public val ZERO_XTEA_KEY_PROVIDER: XteaProvider = XteaProvider { XteaKey.ZERO }
+    }
 }
