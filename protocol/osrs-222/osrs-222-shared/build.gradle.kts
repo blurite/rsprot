c6006892a6a43bfa320946778282f4c1a5d08032
Kris <kristo190@hotmail.com>
2024-05-27 20:04:01 +0300

    refactor: move revision specific modules under a sub module

---
+++ protocol/osrs-222/osrs-222-shared/build.gradle.kts
@@ -0,0 +1,20 @@
+dependencies {
+    implementation(platform(rootProject.libs.netty.bom))
+    implementation(rootProject.libs.netty.buffer)
+    implementation(rootProject.libs.netty.transport)
+    implementation(projects.buffer)
+    implementation(projects.compression)
+    implementation(projects.crypto)
+    implementation(projects.protocol)
+    implementation(projects.protocol.osrs222.osrs222Model)
+    implementation(projects.protocol.osrs222.osrs222Internal)
+    implementation(projects.protocol.osrs222.osrs222Common)
+}
+
+mavenPublishing {
+    pom {
+        name = "RsProt OSRS 222 Shared"
+        description = "The shared module for revision 222 OldSchool RuneScape networking, " +
+            "offering a set of shared classes that do not depend on a specific client."
+    }
+}
