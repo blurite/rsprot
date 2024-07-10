d23bcda9885e5651f786d1c44cec5f6f636a169e
Kris <kristo190@hotmail.com>
2024-07-10 14:58:23 +0300

    fix: cam lookat eased angle absolute generic type

--- protocol/osrs-222/osrs-222-desktop/src/main/kotlin/net/rsprot/protocol/game/outgoing/codec/camera/CamLookAtEasedAngleAbsoluteEncoder.kt
+++ protocol/osrs-222/osrs-222-desktop/src/main/kotlin/net/rsprot/protocol/game/outgoing/codec/camera/CamLookAtEasedAngleAbsoluteEncoder.kt
@@ -3,19 +3,19 @@ package net.rsprot.protocol.game.outgoing.codec.camera
 import io.netty.channel.ChannelHandlerContext
 import net.rsprot.buffer.JagByteBuf
 import net.rsprot.protocol.ServerProt
-import net.rsprot.protocol.game.outgoing.camera.CamLookAtEasedAngleRelative
+import net.rsprot.protocol.game.outgoing.camera.CamLookAtEasedAngleAbsolute
 import net.rsprot.protocol.game.outgoing.prot.GameServerProt
 import net.rsprot.protocol.message.codec.MessageEncoder
 import net.rsprot.protocol.metadata.Consistent
 
 @Consistent
-public class CamLookAtEasedAngleAbsoluteEncoder : MessageEncoder<CamLookAtEasedAngleRelative> {
+public class CamLookAtEasedAngleAbsoluteEncoder : MessageEncoder<CamLookAtEasedAngleAbsolute> {
     override val prot: ServerProt = GameServerProt.CAM_LOOKAT_EASED_ANGLE_ABSOLUTE
 
     override fun encode(
         ctx: ChannelHandlerContext,
         buffer: JagByteBuf,
-        message: CamLookAtEasedAngleRelative,
+        message: CamLookAtEasedAngleAbsolute,
     ) {
         buffer.p2(message.yAngle)
         buffer.p2(message.xAngle)
