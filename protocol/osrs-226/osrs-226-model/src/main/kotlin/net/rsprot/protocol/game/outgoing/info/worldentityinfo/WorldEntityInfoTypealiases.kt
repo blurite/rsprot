7ed69cfe59b1650d42367506ab7e72c9bd877b61
Kris <kristo190@hotmail.com>
2024-10-17 01:38:11 +0300

    feat: create migration typealiases for renamed packets

---
+++ protocol/osrs-226/osrs-226-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/worldentityinfo/WorldEntityInfoTypealiases.kt
@@ -0,0 +1,9 @@
+@file:Suppress("ktlint:standard:filename")
+
+package net.rsprot.protocol.game.outgoing.info.worldentityinfo
+
+@Deprecated(
+    message = "Deprecated. Use WorldEntityInfoV3Packet.",
+    replaceWith = ReplaceWith("WorldEntityInfoV3Packet"),
+)
+public typealias WorldEntityInfoPacket = WorldEntityInfoV3Packet
---
+++ protocol/osrs-226/osrs-226-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/misc/player/MiscPlayerTypealiases.kt
@@ -0,0 +1,13 @@
+package net.rsprot.protocol.game.outgoing.misc.player
+
+@Deprecated(
+    message = "Deprecated. Use UpdateStatV2.",
+    replaceWith = ReplaceWith("UpdateStatV2"),
+)
+public typealias UpdateStat = UpdateStatV2
+
+@Deprecated(
+    message = "Deprecated. Use UpdateStatV1.",
+    replaceWith = ReplaceWith("UpdateStatV1"),
+)
+public typealias UpdateStatOld = UpdateStatV1
