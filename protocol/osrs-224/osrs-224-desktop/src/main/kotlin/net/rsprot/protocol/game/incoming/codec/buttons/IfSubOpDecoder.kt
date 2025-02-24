5bac1fc6add0081067e8944f4c2c7386c2345ff4
Kris <kriszenyte@gmail.com>
2025-02-24 18:31:52 +0200

    fix: swap op and subop order for ifsubop packet

--- protocol/osrs-224/osrs-224-desktop/src/main/kotlin/net/rsprot/protocol/game/incoming/codec/buttons/IfSubOpDecoder.kt
+++ protocol/osrs-224/osrs-224-desktop/src/main/kotlin/net/rsprot/protocol/game/incoming/codec/buttons/IfSubOpDecoder.kt
@@ -16,8 +16,8 @@ public class IfSubOpDecoder : MessageDecoder<IfSubOp> {
         val combinedId = buffer.gCombinedId()
         val sub = buffer.g2()
         val obj = buffer.g2()
-        val subop = buffer.g1()
         val op = buffer.g1()
+        val subop = buffer.g1()
         return IfSubOp(
             combinedId,
             sub,
