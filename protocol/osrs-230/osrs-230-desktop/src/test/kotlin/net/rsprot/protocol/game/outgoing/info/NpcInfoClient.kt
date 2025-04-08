d550bf5f10e80df9ebe9ecf6d31215d57231edf2
Kris <kriszenyte@gmail.com>
2025-04-08 20:27:49 +0300

    feat: info tests

--- protocol/osrs-230/osrs-230-desktop/src/test/kotlin/net/rsprot/protocol/game/outgoing/info/NpcInfoClient.kt
+++ protocol/osrs-230/osrs-230-desktop/src/test/kotlin/net/rsprot/protocol/game/outgoing/info/NpcInfoClient.kt
@@ -57,18 +57,18 @@ class NpcInfoClient {
             val index = updatedNpcSlot[i]
             val npc = checkNotNull(cachedNpcs[index])
             var flag = buffer.g1()
-            if ((flag and 0x10) != 0) {
+            if ((flag and 0x40) != 0) {
                 val extra: Int = buffer.g1()
                 flag += extra shl 8
             }
-            if ((flag and 0x400) != 0) {
+            if ((flag and 0x8000) != 0) {
                 val extra: Int = buffer.g1()
                 flag += extra shl 16
             }
-            check(flag and (0x10 or 0x400 or 0x4).inv() == 0) {
+            check(flag and (0x40 or 0x8000 or 0x20).inv() == 0) {
                 "Extended info other than 'say' included!"
             }
-            if (flag and 0x4 != 0) {
+            if (flag and 0x20 != 0) {
                 val text = buffer.gjstr()
                 npc.overheadChat = text
             }
@@ -150,25 +150,23 @@ class NpcInfoClient {
                     val npc = checkNotNull(cachedNpcs[index])
                     npcSlot[npcSlotCount++] = index
                     npc.lastUpdateCycle = cycle
-
-                    // --
+                    val deltaX = decodeDelta(large, buffer)
+                    val hasSpawnCycle = buffer.gBits(1) == 1
+                    if (hasSpawnCycle) {
+                        npc.spawnCycle = buffer.gBits(32)
+                    }
+                    npc.id = buffer.gBits(14)
+                    val extendedInfo = buffer.gBits(1)
+                    if (extendedInfo == 1) {
+                        updatedNpcSlot[updatedNpcSlotCount++] = index
+                    }
+                    val deltaZ = decodeDelta(large, buffer)
                     val angle = NPC_TURN_ANGLES[buffer.gBits(3)]
                     if (isNew) {
                         npc.turnAngle = angle
                         npc.angle = angle
                     }
-                    val hasSpawnCycle = buffer.gBits(1) == 1
-                    if (hasSpawnCycle) {
-                        npc.spawnCycle = buffer.gBits(32)
-                    }
                     val jump = buffer.gBits(1)
-                    val deltaX = decodeDelta(large, buffer)
-                    val deltaZ = decodeDelta(large, buffer)
-                    val extendedInfo = buffer.gBits(1)
-                    if (extendedInfo == 1) {
-                        updatedNpcSlot[updatedNpcSlotCount++] = index
-                    }
-                    npc.id = buffer.gBits(14)
                     // reset bas
                     if (npc.turnSpeed == 0) {
                         npc.angle = 0
