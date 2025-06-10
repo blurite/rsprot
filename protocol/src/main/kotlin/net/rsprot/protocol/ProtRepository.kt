7725d4a5806ef2f4bb97c34f98265de9423c63b5
Kris <kriszenyte@gmail.com>
2025-06-10 21:22:45 +0300

    fix: skip trying to append prots with opcode -1 to repository

--- protocol/src/main/kotlin/net/rsprot/protocol/ProtRepository.kt
+++ protocol/src/main/kotlin/net/rsprot/protocol/ProtRepository.kt
@@ -15,6 +15,9 @@ public class ProtRepository<T : Prot> internal constructor(
             val entries = enumEntries<P>()
             val builder = ProtRepositoryBuilder<P>(256)
             for (entry in entries) {
+                // Skip the packets with opcode -1 - this implies the packet cannot be sent directly,
+                // and must be part of another packet (like partial enclosed zone updates)
+                if (entry.opcode == -1) continue
                 builder.put(entry)
             }
             return builder.build()
