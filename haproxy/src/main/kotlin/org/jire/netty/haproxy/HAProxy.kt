485490a3e0ad3d95aa51780999949253ccd6aa96
Jire <thomasgnappo@gmail.com>
2025-09-04 13:45:38 -0400

    fix: `HAProxy.childHandlerProxied` pass `mode`/`idleTimeout`/`idleTimeoutUnit` to underlying `HAProxyChannelInitializer`

--- haproxy/src/main/kotlin/org/jire/netty/haproxy/HAProxy.kt
+++ haproxy/src/main/kotlin/org/jire/netty/haproxy/HAProxy.kt
@@ -3,6 +3,9 @@ package org.jire.netty.haproxy
 import io.netty.bootstrap.ServerBootstrap
 import io.netty.channel.Channel
 import io.netty.channel.ChannelInitializer
+import org.jire.netty.haproxy.HAProxyIdleStateHandler.Companion.DEFAULT_IDLE_TIMEOUT
+import org.jire.netty.haproxy.HAProxyIdleStateHandler.Companion.DEFAULT_IDLE_TIMEOUT_UNIT
+import java.util.concurrent.TimeUnit
 
 /**
  * Utilities for working with [HAProxy](https://en.wikipedia.org/wiki/HAProxy).
@@ -12,10 +15,12 @@ public object HAProxy {
     @JvmOverloads
     public fun <C : Channel> ServerBootstrap.childHandlerProxied(
         initializer: ChannelInitializer<C>,
-        haProxyMode: HAProxyMode = HAProxyMode.AUTO,
+        mode: HAProxyMode = HAProxyMode.AUTO,
+        idleTimeout: Long = DEFAULT_IDLE_TIMEOUT,
+        idleTimeoutUnit: TimeUnit = DEFAULT_IDLE_TIMEOUT_UNIT,
     ): ServerBootstrap =
         apply {
-            when (haProxyMode) {
+            when (mode) {
                 HAProxyMode.OFF -> {
                     childHandler(
                         initializer,
@@ -24,7 +29,12 @@ public object HAProxy {
 
                 HAProxyMode.ON, HAProxyMode.AUTO -> {
                     childHandler(
-                        HAProxyChannelInitializer(initializer),
+                        HAProxyChannelInitializer(
+                            initializer,
+                            mode,
+                            idleTimeout,
+                            idleTimeoutUnit,
+                        ),
                     )
                 }
             }
