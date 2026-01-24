bffecc16e32d52504662a70e1dbd446107a4fe4d
Kris <kriszenyte@gmail.com>
2026-01-24 10:40:15 +0200

    fix: world entity priority order

--- protocol/osrs-235/osrs-235-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/worldentityinfo/WorldEntityPriority.kt
+++ protocol/osrs-235/osrs-235-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/worldentityinfo/WorldEntityPriority.kt
@@ -3,11 +3,13 @@ package net.rsprot.protocol.game.outgoing.info.worldentityinfo
 /**
  * An enum of possible world entity render priorities.
  * These dictate the "pass" in which the given worldentity will be drawn in the scene.
+ * Note that the [OTHER_PLAYER] priority has a special exception in the client which makes it
+ * render the entity as a greyed out shadow if a higher priority world overlaps with it.
  */
 public enum class WorldEntityPriority(
     public val id: Int,
 ) {
-    OTHER_PLAYER(0),
-    NPC(1),
+    NPC(0),
+    OTHER_PLAYER(1),
     LOCAL_PLAYER(2),
 }
