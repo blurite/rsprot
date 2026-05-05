de78d69637bb4a765e50ae0760d4f29104eb0c39
Kris <kriszenyte@gmail.com>
2026-05-06 01:36:58 +0300

    feat: update worldentity info

--- protocol/osrs-238/osrs-238-desktop/src/main/kotlin/net/rsprot/protocol/game/outgoing/codec/worldentity/extendedinfo/WorldEntitySequenceEncoder.kt
+++ protocol/osrs-238/osrs-238-desktop/src/main/kotlin/net/rsprot/protocol/game/outgoing/codec/worldentity/extendedinfo/WorldEntitySequenceEncoder.kt
@@ -17,8 +17,8 @@ public class WorldEntitySequenceEncoder : PrecomputedExtendedInfoEncoder<Sequenc
             alloc
                 .buffer(3, 3)
                 .toJagByteBuf()
-        buffer.p2Alt1(extendedInfo.id.toInt())
-        buffer.p1Alt3(extendedInfo.delay.toInt())
+        buffer.p2Alt3(extendedInfo.id.toInt())
+        buffer.p1(extendedInfo.delay.toInt())
         return buffer
     }
 }
--- protocol/osrs-238/osrs-238-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/worldentityinfo/WorldEntityInfo.kt
+++ protocol/osrs-238/osrs-238-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/info/worldentityinfo/WorldEntityInfo.kt
@@ -419,10 +419,10 @@ public class WorldEntityInfo internal constructor(
             val fineZOffset = rootBuildArea.zoneZ shl 10
 
             buffer.p2(avatar.index)
-            buffer.p2Alt1(avatar.id)
-            buffer.p1Alt3(priority.id)
-            buffer.p1Alt3((avatar.sizeX shl 4) or avatar.sizeZ)
             val placeholderFlag = pPlaceholderExtendedInfoFlag(buffer)
+            buffer.p2Alt1(avatar.id)
+            buffer.p1Alt2(priority.id)
+            buffer.p1((avatar.sizeX shl 4) or avatar.sizeZ)
             buffer.encodeAngledCoordFine(
                 avatar.currentCoordFine.x - fineXOffset,
                 avatar.currentCoordFine.y,
