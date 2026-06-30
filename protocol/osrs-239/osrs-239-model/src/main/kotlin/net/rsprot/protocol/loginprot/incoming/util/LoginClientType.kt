788481dd6b0fc2d4d84d3e8d5552f0adf479117e
Kris <kriszenyte@gmail.com>
2026-06-30 13:43:35 +0300

    feat: allow legacy android and ios to connect as desktop clients

--- protocol/osrs-239/osrs-239-model/src/main/kotlin/net/rsprot/protocol/loginprot/incoming/util/LoginClientType.kt
+++ protocol/osrs-239/osrs-239-model/src/main/kotlin/net/rsprot/protocol/loginprot/incoming/util/LoginClientType.kt
@@ -18,12 +18,16 @@ public enum class LoginClientType(
     public fun toOldSchoolClientType(): OldSchoolClientType? {
         return when (this) {
             DESKTOP -> OldSchoolClientType.DESKTOP
+            // Non-enhanced Android & IOS are no longer in use, but this helps
+            // RSPS use ghetto mobile and distinguish it.
+            ANDROID -> OldSchoolClientType.DESKTOP
+            IOS -> OldSchoolClientType.DESKTOP
+
             ENHANCED_WINDOWS -> OldSchoolClientType.DESKTOP
             ENHANCED_LINUX -> OldSchoolClientType.DESKTOP
             ENHANCED_MAC -> OldSchoolClientType.DESKTOP
             ENHANCED_ANDROID -> OldSchoolClientType.ANDROID
             ENHANCED_IOS -> OldSchoolClientType.IOS
-            else -> null
         }
     }
 
