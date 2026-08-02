d943bd4e5dd7b00e169594a1f6235285340a72e3
Kris <kriszenyte@gmail.com>
2026-08-03 01:37:56 +0300

    fix: migrate all send_ping packets to high priority group

--- protocol/osrs-222/osrs-222-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/misc/client/SendPing.kt
+++ protocol/osrs-222/osrs-222-model/src/main/kotlin/net/rsprot/protocol/game/outgoing/misc/client/SendPing.kt
@@ -19,7 +19,7 @@ public class SendPing(
     public val value2: Int,
 ) : OutgoingGameMessage {
     override val category: ServerProtCategory
-        get() = GameServerProtCategory.LOW_PRIORITY_PROT
+        get() = GameServerProtCategory.HIGH_PRIORITY_PROT
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
